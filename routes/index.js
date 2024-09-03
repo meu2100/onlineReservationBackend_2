c2dbc53c9c07d29bc70a6f18d2e49df55a4c3437
meu2100 <amanda8018@gmail.com>
2024-09-03 17:20:36 +0800

    feat: add get router

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const { Service, Staff } = require("../models");
 
+router.get("/:companyName/services", (req, res) => {
+  return Service.findAll({
+    raw: true,
+  })
+    .then((services) => {
+      res.json(services);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Failed to read file" });
+    });
+});
 
-module.exports = router
+router.get("/:companyName/services/:id", (req, res) => {
+  return Service.findByPk(req.params.id, {
+    raw: true,
+  })
+    .then((service) => {
+      if(!service) throw new ERROR ("Service do not exist!");
+      res.json(service);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Failed to read file" });
+    });
+});
+
+router.get("/staff", (req, res) => {
+  return Staff.findAll({
+    raw: true,
+  })
+    .then((Staffs) => {
+      res.json(Staffs);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Failed to read file" });
+    });
+});
+
+module.exports = router;
